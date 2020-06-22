6486c9f82e1d562b1954d69ac953619eafc0264b
RissaSams <rissa.sams@yahoo.com>
2020-06-22 10:01:35 -0500

    new

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const createList = document.getElementById("createList");
+
+//     createList.addEventListener("click", function() {
+//         let createListName = prompt("Enter a list name");
+
+//         const taskListName = document.getElementById("TaskListName");
+//         taskListName.innerText = createListName;
+//     })
+// })
+
 document.addEventListener("DOMContentLoaded", function() {
     const createList = document.getElementById("createList");
 
-    createList.addEventListener("click", function () {
-        let creatListName = promt("Enter a name");
+    createList.addEventListener("click", function() {
+        const createListName = document.getElementById("createListName").value;
 
-        const taskListName = document.getElementById("TaskLisName");
-        taskListName.innerText = createListName
-        
+        const taskListName = document.getElementById("TaskListName");
+        taskListName.innerText = createListName;
+        let listOfTasks = document.createElement("ul");
+        let task = document.createElement("li");
+        let taskDiv = document.createElement("div");
+        taskDiv.contentEditable = true;
+        task.appendChild(taskDiv);
+        listOfTasks.appendChild(task);
+        document.getElementById("TaskList").appendChild(listOfTasks);
+
     })
-
 })
-
-    
-
